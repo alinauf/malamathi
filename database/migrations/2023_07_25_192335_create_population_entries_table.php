ba648e8471d2d432ea3b29ada41d49491f967b25
Ali Nauf <naufpsychic@gmail.com>
2023-07-26 00:39:34 +0500

    feat: add migrations for Atoll, Island Category, Island and Population Entry

---
+++ database/migrations/2023_07_25_192335_create_population_entries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('population_entries', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('atoll_id')->constrained();
+            $table->integer('island_id');
+            $table->unsignedInteger('men_count');
+            $table->unsignedInteger('women_count');
+            $table->unsignedInteger('local_count');
+            $table->unsignedInteger('expat_count');
+            $table->unsignedInteger('total_population');
+            $table->string('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('population_entries');
+    }
+};
