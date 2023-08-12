46de2a7e32111bf5ddf3e2bfa7270d11221fac6e
weop <weop@users.noreply.github.com>
2023-08-12 13:07:00 +0500

    +about page basic

--- resources/views/frontend/home/includes/about.blade.php
+++ resources/views/frontend/home/includes/about.blade.php
@@ -1,19 +1,31 @@
   <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
   
     <div class="flex flex-col md:flex-row py-5 m-5 text-left max-w gap-6">
-        <div class="md:w-2/3 px-6 pb-6 bg-blue-50 rounded">
-            <h1 class="text-2xl my-4 font-serif font-bold text-gray-dark text-left pb-0">Our Mission</h1>
-            <p class="lead">To protect, preserve and pass it to the next generation we need to know what’s out there and what changes are occurring to its ecosystems due to climate change and other factors! The aim of Malamathi is to empower communities and individuals to take part in documenting the ecosystems of Maldives. Share with us and take part in this community effort to document the Maldives and changes to its ecosystems over time!</p>
+        <div class="md:w-1/4 px-6 pb-6 bg-blue-50 rounded">
+                <h1 class="text-xl my-4 font-serif text-gray-dark text-left pb-0 pt-3">Preserving Natural Beauty of Maldives, Together</h1>
         </div>
+        <div class="md:w-3/4 px-6 pb-6 bg-gray-50 rounded">
+            <p class="lead text-justify">
+            <h1 class="text-3xl my-4 font-serif text-gray-dark text-left pb-0 pt-3">Malamathi</h1>
 
-        <div class="md:w-1/3 px-6 pb-6 bg-gray-50 rounded">
-            <h1 class="text-4xl my-2 font-serif font-bold text-gray-dark text-left pb-0 pt-4">"</h1>
-            <p class="lead text-lg text-center">
-            We strive to document and preserve the beauty and biodiversity of the Maldives for future generations
+            <p class="py-3 text-justify">Welcome to Malamathi, a community-driven initiative committed to safeguarding the breathtaking natural ecosystems of the Maldives. Our purpose is clear: to empower individuals and communities, uniting them in the protection of this paradise. Our mission involves active engagement, documentation, and raising awareness about the challenges posed by climate change and other human-driven factors.</p>
+
+            <p class="py-3 text-justify"><b>Our Approach</b><br>
+            At Malamathi, we're on a journey to enable everyone to become stewards of the Maldives' ecosystems. By providing a platform, we encourage people to share their personal experiences and insights about this incredible corner of the world. Through collaboration, we hope to address environmental issues as a united front.</p>
+
+            <p class="py-3 text-justify"><b>Our Vision</b><br>
+            We strongly believe that through careful documentation of the changes our ecosystems undergo, we'll gain invaluable insights. These insights will help us comprehend the challenges that lie ahead. By working in harmony, we can effectively tackle these challenges and ensure the continuity of our invaluable resources.</p>
+
+            <p class="py-3 text-justify"><b>Connect and Contribute</b><br>
+            Join us in our collective endeavor to protect the Maldives' ecosystems for generations to come. Your participation matters. Together, we hold the power to drive positive change and create a lasting impact.</p>
+
+            <p class="py-3 text-justify"><b>Let's Work Together</b><br>
+            Malamathi invites you to be a part of something greater. It's time to step up, share your perspective, and take action. By preserving the Maldives' natural beauty, we're not only safeguarding our present but also building a sustainable legacy for the future.</p>
+
+            <p class="py-3 text-justify">Come, join us on this meaningful journey. Together, we can make a difference that echoes through time.</p>
+
             </p>
-            <h1 class="text-4xl font-serif font-bold text-gray-dark text-right pb-0">"</h1>
         </div>
-    </div>
 
 
 </div>
