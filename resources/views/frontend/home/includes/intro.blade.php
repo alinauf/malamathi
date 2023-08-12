52a62e1163e7ee0ddc7f3b73544bebf415921203
weop <weop@users.noreply.github.com>
2023-08-12 10:12:40 +0500

    slider animation

--- resources/views/frontend/home/includes/intro.blade.php
+++ resources/views/frontend/home/includes/intro.blade.php
@@ -1,12 +1,15 @@
   <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
 
-    <div class="flex flex-col md:flex-row py-5 my-5 text-left max-w gap-0">
-    <div class="align-items-center p-4">
-    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-        <lottie-player autoplay="" background="transparent" loop="" speed="0.4" src="/animation_home-f1ba6690b76284a1e314439f0eb41dbf4216a0bc3d6ce4bbe2f671daacfb9114.json" style="width: 900px; height: auto;"></lottie-player>
+    <div class="flex flex-col md:flex-row py-5 m-5 text-left max-w gap-6">
+        <div class="md:w-2/3 px-6 pb-6 bg-gray-50">
+        <div class="introimage-container">
+        <div class="introimage">
+            <img src="/addu_by_aishath_naj.png" alt="Intro Image">
+        </div>
+        </div>
     </div>
 
-    <div class="align-items-center p-4">
+    <div class="md:w-1/3 px-6 pb-6 bg-gray-50">
         <h1 class="text-2xl my-4 font-serif font-bold text-gray-dark  py-5">Let’s Document our Raajje!</h1>
 
         <p class="lead text-xl my-6">
@@ -21,12 +24,12 @@
 
     <div class="flex flex-col md:flex-row py-5 m-5 text-left max-w gap-6">
         <div class="md:w-2/3 px-6 pb-6 bg-gray-50">
-            <h1 class="text-2xl my-4 font-serif font-bold text-gray-dark text-center pb-0">Our Mission</h1>
+            <h1 class="text-2xl my-4 font-serif font-bold text-gray-dark text-left pb-0">Our Mission</h1>
             <p class="lead">To protect, preserve and pass it to the next generation we need to know what’s out there and what changes are occurring to its ecosystems due to climate change and other factors! The aim of Malamathi is to empower communities and individuals to take part in documenting the ecosystems of Maldives. Share with us and take part in this community effort to document the Maldives and changes to its ecosystems over time!</p>
         </div>
 
         <div class="md:w-1/3 px-6 pb-6 bg-gray-50">
-            <h1 class="text-2xl my-4 font-serif font-bold text-gray-dark text-center pb-0">Contact Us</h1>
+            <h1 class="text-2xl my-4 font-serif font-bold text-gray-dark text-left pb-0">Contact Us</h1>
             <p class="lead">
             H. V Kanmmathi 10, 10136 Turin Male', Maldives <br>
             Phone: (0039) 333 12 68 347 <br>
@@ -38,3 +41,52 @@
 
 
 </div>
+
+<style>
+  .introimage-container {
+    width: 100%;
+    overflow: hidden;
+    position: relative;
+  }
+
+  .introimage {
+    width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  .introimage img {
+    width: 100%;
+    display: block;
+  }
+
+  .introimage:before{
+    content:'';
+    position:absolute;
+    top:0;
+    left:0;
+    width:150px;
+    height:100%;
+    border-left:2px solid rgba(255,255,255,0.7);
+    border-right:2px solid rgba(255,255,255,0.7);
+    background:url('/addu_by_aishath_naj.png');
+    background-attachment:fixed;
+    background-position:bottom;
+    background-repeat:no-repeat;
+    background-size: 100% 100%;
+    box-shadow:20px 0 100px rgba(0,0,0,0.2),
+                -20px 0 100px rgba(0,0,0,0.2);
+    background-size:110%;
+    animation: slideAnimate 10s ease-in-out infinite; 
+  }
+
+@keyframes slideAnimate{
+  0%,100%{
+    left:0px;
+  }
+  
+  50%{
+    left:calc(100%);
+  }
+}
+</style>
