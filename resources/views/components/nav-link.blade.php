09cd7e83d96b655bfd19262f54677ca63a12e7ff
weop <weop@users.noreply.github.com>
2023-08-12 14:21:07 +0500

    nav-hover

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,10 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-blue-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-blue-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-blue-400 px-3 text-md font-medium leading-5 
+                    text-gray-900 focus:outline-none hover:bg-blue-50 focus:border-blue-700 transition duration-150 ease-in-out'
+            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent px-3 text-md font-medium leading-5 
+                    text-gray-600 hover:text-white hover:bg-blue-800 focus:outline-none focus:text-gray-300 focus:border-green-300 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
